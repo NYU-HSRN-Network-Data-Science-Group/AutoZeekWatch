e9e8082ef7a2ae72b88ef7424c4707aacb4c6270
Jiayuan Song <js10417@nyu.edu>
2024-02-13 08:13:25 -0500

    Revert "Merge pull request #10 from zoe70416/olive4"
    This reverts commit 25e10f6a1666191b3c3513ef55ed957899bccd1a, reversing
    changes made to 13d85932b1c9a5e8acee9a0a1d552b8a1fb6f190.
    accidentally performed merge before view lol

--- NIDS/train.py
+++ NIDS/train.py
@@ -52,14 +52,8 @@ def train_batch(kit, np_arr):
     function so please instantiate it somewhere. Keep it in memory and right before 
     main exits we dump it to disk.
     """
-    # logging.info("Hello from train_batch. Please implement me :)")
+    logging.info("Hello from train_batch. Please implement me :)")
     # TODO: run partial fit over the np_arr
-    
-    assert len(np_arr.shape) <= 2, "The input array must be shape (n_samples, n_features) or (n_features,)."     
-    kit.fit(np_arr) 
-
-    return kit 
-
 
 def main():
     """
@@ -95,6 +89,9 @@ def main():
                     np_arr = preprocess_json(json_data_file)
                     train_batch(kit, np_arr)
     # TODO: Before we exit the main function, dump the trained model to disk
+
+
+
 
 
 
